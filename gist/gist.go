b4017e4cb78b190cfd60a7851fa0127aa7a8ba01
bayram <bay00@outlook.de>
2020-03-25 17:38:52 +0100

    added toast notification

--- gist/gist.go
+++ gist/gist.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"io/ioutil"
 	"os"
+	"time"
 
 	"github.com/google/go-github/github"
 	"github.com/spf13/viper"
@@ -20,6 +21,26 @@ func Create(client *github.Client, configFilepath string, filename string) (*git
 	f[github.GistFilename(filename)] = github.GistFile{Content: github.String(string(bytes))}
 	gist := &github.Gist{
 		Description: github.String(viper.GetString("defaultDescription")),
+		Public:      github.Bool(viper.GetBool("public")),
+		Files:       f,
+	}
+	gistResponse, _, err := client.Gists.Create(ctx, gist)
+	return gistResponse, err
+}
+
+func CreateBackup(client *github.Client, csgopath string, configs []string) (*github.Gist, error) {
+	currentTime := time.Now()
+	ctx := context.Background()
+	f := make(map[github.GistFilename]github.GistFile)
+	for _, config := range configs {
+		bytes, err := ioutil.ReadFile(csgopath + `\ev0lve\` + config)
+		if err != nil {
+			os.Exit(1)
+		}
+		f[github.GistFilename(config)] = github.GistFile{Content: github.String(string(bytes))}
+	}
+	gist := &github.Gist{
+		Description: github.String("Backup: " + currentTime.Format("2006-01-02 15:04:05 Monday")),
 		Public:      github.Bool(viper.GetBool("public")),
 		Files:       f,
 	}
--- go.mod
+++ go.mod
@@ -5,19 +5,17 @@ go 1.13
 require (
 	github.com/andygrunwald/vdf v1.0.0
 	github.com/atotto/clipboard v0.1.2
-	github.com/fsnotify/fsnotify v1.4.7
+	github.com/fsnotify/fsnotify v1.4.9
 	github.com/gizak/termui/v3 v3.1.0
 	github.com/google/go-github v17.0.0+incompatible
 	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/mattn/go-colorable v0.1.6 // indirect
 	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
-	github.com/onsi/ginkgo v1.12.0 // indirect
-	github.com/onsi/gomega v1.9.0 // indirect
-	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
 	github.com/sirupsen/logrus v1.5.0
 	github.com/spf13/viper v1.6.2
 	github.com/x-cray/logrus-prefixed-formatter v0.5.2
-	golang.org/x/crypto v0.0.0-20200323165209-0ec3e9974c59 // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd
+	gopkg.in/toast.v1 v1.0.0-20180812000517-0a84660828b2
 )
